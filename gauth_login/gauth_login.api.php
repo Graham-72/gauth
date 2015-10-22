ccf4923d147aa55943c72ccf82a618a582900096
Graham-72 <g.oliver@btinternet.com>
2015-10-22 06:31:27 +0100

    Port to Backdrop - first pass

--- gauth_login/gauth_login.api.php
+++ gauth_login/gauth_login.api.php
@@ -17,13 +17,13 @@
  *   The 'userinfo' array from OAuth.
  *
  * @return object|NULL
- *   An existing Drupal user object if found; otherwise NULL.
+ *   An existing Backdrop user object if found; otherwise NULL.
  */
 function hook_gauth_login_find_existing_user($info) {
   // Check to see if the user exists in a 3rd party system, ex. LDAP.
   if ($remote_user_object = remote_find_user($info['email'])) {
-    // If so, we jumpstart creation of the local Drupal user and return it.
-    return remote_create_drupal_user($remote_user_object);
+    // If so, we jumpstart creation of the local Backdrop user and return it.
+    return remote_create_backdrop_user($remote_user_object);
   }
 }
 
